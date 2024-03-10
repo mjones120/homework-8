53fffd2a10fb3a3372f1f21169d7e4df13e7cf77
mjones120 <157424025+mjones120@users.noreply.github.com>
2024-03-10 18:31:13 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -14,6 +14,19 @@ use app\controllers\PostController;
 $url = $_SERVER["REQUEST_URI"];
 
 //todo add a switch statement router to route based on the url
+switch($url) 
+{
+  case '/posts':
+  
+    
+  case '/':
+    return
+  default:
+  require_once '404 view';
+    
+  }
+  
+  
 //if it is "/posts" return an array of posts via the post controller
 //if it is "/" return the homepage view from the main controller
 //if it is something else return a 404 view from the main controller
