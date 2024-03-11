f333465105cf9193069a5b238753664c4be39357
mjones <you@example.com>
2024-03-11 17:43:01 -0400

    'main'

--- app/models/Post.php
+++ app/models/Post.php
@@ -4,5 +4,20 @@ namespace app\models;
 
 class Post
 {
+    public function getAllPosts()
+    {   
+        return [
+            [
+                'id' => '1',
+                'title' => 'first post',
+                'views' => '6'
+            ],
+            [
+                'id' => '2',
+                'title' => 'second post',
+                'views' => '5'
+            ]
+        ];
 
+    }
 }
